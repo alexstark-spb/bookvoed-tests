68c62bab5b388022610c7227653c7ea8214d293f
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-11-21 16:31:59 +0300

    added class BookvoedTest & TestBase & telegram.json

---
+++ src/test/java/com/alexstark/tests/BookvoedTest.java
@@ -0,0 +1,238 @@
+package com.alexstark.tests;
+
+import com.alexstark.domain.MenuItem;
+import com.alexstark.pages.*;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+
+
+public class BookvoedTest extends TestBase {
+
+    private AuthorizationPage authorizationPage = new AuthorizationPage();
+    private SearchPage searchPage = new SearchPage();
+    private CartPage cartPage = new CartPage();
+    private CatalogPage catalogPage = new CatalogPage();
+    private MainPage mainPage = new MainPage();
+
+
+    @Test
+    @Feature("Authorization")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Авторизация с корректными данными")
+    @Tag("Authorization")
+    @Disabled //UI-тест который не пройдет из-за появления капчи
+    void authorization() {
+        authorizationPage
+                .openPage()
+                .openLoginPage()
+                .typeEmail()
+                .typePassword()
+                .typeSubmit()
+                .checkProfileName();
+    }
+
+    @Test
+    @Feature("Authorization")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Авторизация с некорректными данными")
+    @Tag("Authorization")
+    void authorizationWithIncorrectData() {
+        authorizationPage
+                .openPage()
+                .openLoginPage()
+                .typeEmailWithFaker()
+                .typePasswordWithFaker()
+                .typeSubmit()
+                .checkProfileName();
+    }
+
+    @Feature("Search")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Поиск товаров и применение фильтрации по цене")
+    @CsvSource(value = {
+            "451 градус по фаренгейту, 350",
+            "Дюна, 200",
+            "Над пропастью во ржи, 200"
+
+    })
+    @ParameterizedTest(name = "Найти товар : {0}")
+    @Tag("SearchAndCatalog")
+    void findProductInSearch(String product, String price) {
+        searchPage.openPage();
+        if (searchPage.checkDisplayedRegion()) {
+            searchPage.closePopUpRegion()
+                    .typeSearchField(product)
+                    .typeMinPriceField(price)
+                    .checkTitlePageFilter();
+        } else {
+            searchPage
+                    .typeSearchField(product)
+                    .typeMinPriceField(price)
+                    .checkTitlePageFilter();
+        }
+    }
+
+    @Test
+    @Feature("Catalog")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Переход по каталогу и открытие PDP-карточки товара")
+    @Tag("SearchAndCatalog")
+    void openCatalogAndPDP() {
+        catalogPage.openPage();
+        if (catalogPage.checkDisplayedRegion()) {
+            catalogPage.closePopUpRegion()
+                    .typeCatalogTab()
+                    .typeSubdirectoryTab()
+                    .typeSubcategory()
+                    .typeFirstPopularGoods()
+                    .switchToSecondTabBrowser()
+                    .checkBuyButtonInMenu();
+        } else {
+            catalogPage
+                    .typeCatalogTab()
+                    .typeSubdirectoryTab()
+                    .typeSubcategory()
+                    .typeFirstPopularGoods()
+                    .switchToSecondTabBrowser()
+                    .checkBuyButtonInMenu();
+        }
+    }
+
+    @Test
+    @Feature("Cart")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Добавление товара в корзину со страницы PDP")
+    @Tag("Cart")
+    void addToCartFromPDP() {
+        cartPage.openPage();
+        if (cartPage.checkDisplayedRegion()) {
+            cartPage.closePopUpRegion()
+                    .typeFirstProduct()
+                    .switchToSecondTabBrowser()
+                    .typeCartButton()
+                    .typeButtonInPopUP()
+                    .checkNameCart()
+                    .checkAreaGoods();
+        } else {
+            cartPage
+                    .typeFirstProduct()
+                    .switchToSecondTabBrowser()
+                    .typeCartButton()
+                    .typeButtonInPopUP()
+                    .checkNameCart()
+                    .checkAreaGoods();
+        }
+    }
+
+    @Test
+    @Feature("Cart")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Добавление товара в корзину со страницы PLP")
+    @Tag("Cart")
+    void addToCartFromPLP() {
+        cartPage.openPage();
+        if (cartPage.checkDisplayedRegion()) {
+            cartPage.closePopUpRegion()
+                    .typeCartButtonPLP()
+                    .typeButtonInPopUP()
+                    .checkNameCart()
+                    .checkAreaGoods();
+        } else {
+            cartPage
+                    .typeCartButtonPLP()
+                    .typeButtonInPopUP()
+                    .checkNameCart()
+                    .checkAreaGoods();
+        }
+    }
+
+    @Test
+    @Feature("Cart")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Очистка корзины после добавления в неё товара")
+    @Tag("Cart")
+    void clearCart() {
+        cartPage.openPage();
+        if (cartPage.checkDisplayedRegion()) {
+            cartPage.closePopUpRegion()
+                    .typeFirstProduct()
+                    .switchToSecondTabBrowser()
+                    .typeCartButton()
+                    .typeButtonInPopUP()
+                    .typeButtonClear()
+                    .checkCartEmpty();
+        } else {
+            cartPage
+                    .typeFirstProduct()
+                    .switchToSecondTabBrowser()
+                    .typeCartButton()
+                    .typeButtonInPopUP()
+                    .typeButtonClear()
+                    .checkCartEmpty();
+        }
+    }
+
+    @Feature("MainPage")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Переход между вкладками на тапбаре главной страницы")
+    @Tag("MainPage")
+    @EnumSource(value = MenuItem.class)
+    @ParameterizedTest(name = "Открыть вкладку : {0}")
+    void openTabs(MenuItem menuItem) {
+        mainPage.openPage();
+        if (mainPage.checkDisplayedRegion()) {
+            mainPage.closePopUpRegion()
+                    .openHeaderTabs(menuItem)
+                    .checkNameTab(menuItem);
+        } else {
+            mainPage
+                    .openHeaderTabs(menuItem)
+                    .checkNameTab(menuItem);
+        }
+    }
+
+    @Test
+    @Feature("Shops")
+    @Owner("Alexander Derevyanko")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(name = "bookvoed", url = "https://www.bookvoed.ru/")
+    @DisplayName("Просмотр списка магазинов")
+    @Tag("MainPage")
+    void viewListOfStores() {
+        mainPage.openPage();
+        if (mainPage.checkDisplayedRegion()) {
+            mainPage.closePopUpRegion()
+                    .typeShopsTab()
+                    .goToShopListTab()
+                    .checkListShops();
+        } else {
+            mainPage
+                    .typeShopsTab()
+                    .goToShopListTab()
+                    .checkListShops();
+        }
+    }
+}
---
+++ src/test/java/com/alexstark/tests/TestBase.java
@@ -0,0 +1,41 @@
+package com.alexstark.tests;
+
+import com.alexstark.helpers.DriverSettings;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.alexstark.helpers.AllureAttachments.addVideo;
+import static io.qameta.allure.Allure.step;
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase extends DriverSettings {
+
+    @BeforeAll
+    static void setupBeforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+        attachAsText("BrowserRun",System.getProperty("browserName","chrome"));
+    }
+
+    @BeforeEach
+    public void setupBeforeEach() throws InterruptedException {
+        if (System.getProperty("threads") != null) {
+            Thread.sleep(5_000);
+        }
+    }
+
+    @AfterEach
+    public void setupAfterEach() {
+        screenshotAs("Last screenshot");
+        pageSource();
+        browserConsoleLogs();
+        addVideo();
+        step("Закрытие браузера", Selenide::closeWebDriver);
+    }
+}
